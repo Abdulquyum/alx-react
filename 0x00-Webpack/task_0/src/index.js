72da1e9ae1b434827bb11bb10a0963163ffc3ed7
Abdulquyum <ajumobiabdulquyum@gmail.com>
2025-05-17 20:39:00 +0100

    run webpack with jquery and without config file

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,9 @@
+import $ from "jquery";
+
+$(document).ready(function() {
+  $('body').append(`
+    <p>ALX Dashboard</p>
+    <p>Dashboard data for the students</p>
+    <p>Copyright - ALX</p>
+  `);
+});
