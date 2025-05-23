9b69fa3d833eb5117ab64e3bd7976911c8f3dd6d
Abdulquyum <ajumobiabdulquyum@gmail.com>
2025-05-23 11:13:43 +0100

    set up webpack for react project

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,52 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.css$/, 
+        use: ["style-loader", "css-loader"] 
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              optipng: {
+                enabled: true,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              }
+            }
+          }
+        ],
+        type: 'asset/resource',
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack Output",
+    }),
+  ],
+};
